17d0be2357904e2684cb4f6fcf7e78761b4dfa7d
venky0195 <ven.venky08@gmail.com>
2022-08-09 09:42:58 +0530

    Check preview mode and initial props

---
+++ pages/api/preview.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+    // ...
+    res.setPreviewData({})
+    res.end('Preview mode enabled')
+  }
---
+++ pages/using-initial-props.js
@@ -0,0 +1,12 @@
+function UsingInitialProps({ stars }) {
+    return <div>Next stars: {stars}</div>
+  }
+  
+  UsingInitialProps.getInitialProps = async (ctx) => {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const json = await res.json()
+    console.log("stargazers_count",json.stargazers_count);
+    return { stars: json.stargazers_count }
+  }
+  
+  export default UsingInitialProps

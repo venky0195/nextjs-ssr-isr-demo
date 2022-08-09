216e3094761f397dfe8c00578ac5e816c889a44f
venky0195 <ven.venky08@gmail.com>
2022-08-09 16:00:16 +0530

    Add dynamic post route

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,5 +1,13 @@
-export default function handler(req, res) {
-    // ...
-    res.setPreviewData({})
-    res.end('Preview mode enabled')
+export default async (req, res) => {
+    // Check the secret and next parameters
+    // This secret should only be known to this API route and the CMS
+    // if (req.query.secret !== '123') {
+    //   return res.status(401).json({ message: 'Invalid token' })
+    // }
+    // Enable Preview Mode by setting the cookies
+    res.setPreviewData({
+        preview: 123
+    })
+
+    res.redirect(`/posts/${req.query.id}`)
   }
